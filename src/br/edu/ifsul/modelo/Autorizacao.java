3f7e4d935170949c5d8422a0591689878ee6acfb
renatoanunciacao <wagnerpunk002@gmail.com>
2018-06-20 20:34:38 -0300

    Projeto de Locação de filmes

---
+++ src/br/edu/ifsul/modelo/Autorizacao.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.modelo;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ *
+ * @author Renato
+ */
+@Entity
+@Table(name = "autorizacao")
+public class Autorizacao implements Serializable {
+
+    @Id
+    @Column(name = "apelido", length = 30, nullable = false)
+    private String apelido;
+    @NotBlank(message = "Os detalhes devem ser informados")
+    @Length(max = 40, message = "Os detalhes não deve ter mais que {max} caracteres")
+    @Column(name = "descricao", length = 40, nullable = false)
+    private String detalhes;
+
+    public Autorizacao() {
+
+    }
+    
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public String getDetalhes() {
+        return detalhes;
+    }
+
+    public void setDetalhes(String detalhes) {
+        this.detalhes = detalhes;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.apelido);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Autorizacao other = (Autorizacao) obj;
+        if (!Objects.equals(this.apelido, other.apelido)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+}
---
+++ src/br/edu/ifsul/modelo/Pessoa.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.modelo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ *
+ * @author Renato
+ */
+@Entity
+@Table(name = "pessoa")
+public class Pessoa implements Serializable{
+    
+    @Id
+    @SequenceGenerator(name = "seq_pessoa", sequenceName = "seq_pessoa_id", 
+            allocationSize = 1)
+    @GeneratedValue(generator = "seq_pessoa", strategy = GenerationType.SEQUENCE)
+    private Integer codigo;
+     @NotNull(message = "O nome não pode ser nulo")
+    @NotBlank(message = "O nome não pode ser em branco")
+    @Length(max = 50, message = "O nome não pode ter mais que {max} caracteres")
+    @Column(name = "pes_nome", length = 50, nullable = false) 
+    private String nome;
+    @Temporal(TemporalType.DATE)
+    @NotNull(message = "O data nascimento não pode ser nulo")    
+    @Column(name = "pes_nascimento",  nullable = false) 
+    private Calendar data_nascimento;
+     @NotNull(message = "O endereço não pode ser nulo")
+    @NotBlank(message = "O endereço não pode ser em branco")
+    @Length(max = 50, message = "O endereço não pode ter mais que {max} caracteres")
+    @Column(name = "pes_endereco", length = 50, nullable = false) 
+    private String endereco;
+    @NotNull(message = "O nickname não pode ser nulo")
+    @NotBlank(message = "O nickname não pode ser em branco")
+    @Length(max = 20, message = "O nickname não pode ter mais que {max} caracteres")        
+    @Column(name = "nickname", length = 20, nullable = false, unique = true)
+    private String nickname;
+    @NotNull(message = "A chave de acesso não pode ser nulo")
+    @NotBlank(message = "A chave de acesso não pode ser em branco")
+    @Length(max = 20, message = "A chave de acesso não pode ter mais que {max} caracteres")    
+    @Column(name = "chave_acesso", length = 20, nullable = false)
+    private String chaveAcesso;
+    @ManyToMany
+    @JoinTable(name = "autorizacoes",
+            joinColumns
+            = @JoinColumn(name = "pessoa", referencedColumnName = "nickname", nullable = false),
+            inverseJoinColumns
+            = @JoinColumn(name = "autorizacao", referencedColumnName = "apelido", nullable = false),
+            uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "UK_autorizacoes",
+                        columnNames = {"pessoa", "autorizacao"})})
+    List<Autorizacao> autorizacoes = new ArrayList<>();
+
+    public Pessoa(){
+        
+    }
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Calendar getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(Calendar data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getChaveAcesso() {
+        return chaveAcesso;
+    }
+
+    public void setChaveAcesso(String chaveAcesso) {
+        this.chaveAcesso = chaveAcesso;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.codigo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pessoa other = (Pessoa) obj;
+        if (!Objects.equals(this.codigo, other.codigo)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    
+}
